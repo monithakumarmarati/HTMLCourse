d54343af1af5fdf3083b39a53139e46d5995fde8
maratimonitha <kavitha.re1987@gmail.com>
2026-07-19 14:34:13 +0530

    19july

---
+++ project one/src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from "./components/Navbar"
+import Footer from "./components/footer"
+import Hero from "./components/Hero"
+import Features from "./components/features"
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      
+      <Footer />
+
+    </div>
+  )
+}
+
+export default Home
